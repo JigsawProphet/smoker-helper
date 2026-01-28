ca885b26c7df1d4a92f1f736f515360394324ea8
JigsawProphet <goodrich.coding@gmail.com>
2026-01-27 18:56:33 -0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PelletPlanner from './PelletPlanner';
+
+function App() {
+  return (
+    <div className="App">
+      <PelletPlanner />
+    </div>
+  );
+}
+
+export default App;
